3d8c45a63604ee42b295753c487e674b4228f33a
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-13 22:42:58 +0100

    more progress

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [displayTime, setDisplayTime] = useState(25*60);
   const [breakTime, setBreakTime] = useState(5*60);
   const [sessionTime, setSessionTime] = useState(25*60);
+  const [timerOn, setTimerOn] =useState(false);
 
   const formatTime =(time) =>{
     let minutes = Math.floor(time/60);
@@ -16,32 +17,41 @@ function App() {
 
   const changeTime =(amount, type) =>{
     if (type =='break'){
+      if(breakTime <= 60 && amount <0){
+        return;
+      }
       setBreakTime(prev => prev + amount)
     }else{
+      if(sessionTime <= 60 && amount < 0){
+        return;
+      }
       setSessionTime(prev => prev + amount)
+      if(!timerOn){
+        setDisplayTime(sessionTime + amount)
+      }
     }
   }
 
   return (
     <div className="App">
-
-    
+      <h1> POMODORO CLOCK</h1>
+      <div className='comp'>
+        <Length 
+        title={"Break Length"} 
+        changeTime={changeTime} 
+        type={"break"} 
+        time={breakTime} 
+        formatTime={formatTime}
+        />
+      
       <Length 
-      title={"Break Length"} 
-      changeTime={changeTime} 
-      type={"break"} 
-      time={breakTime} 
-      formatTime={formatTime}
-      />
-    
-    <Length 
-      title={"Session Length"} 
-      changeTime={changeTime} 
-      type={"session"} 
-      time={sessionTime} 
-      formatTime={formatTime}
-      />
-
+        title={"Session Length"} 
+        changeTime={changeTime} 
+        type={"session"} 
+        time={sessionTime} 
+        formatTime={formatTime}
+        />
+    </div>
       <h1>{formatTime(displayTime)}</h1>
       
     </div>
