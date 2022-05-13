a0d669a114c3fdc1e929d5f8967c38f46079db42
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-13 14:44:39 +0100

    imported images

--- src/components/Length.js
+++ src/components/Length.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+//importing images
+import pause from '../images/pause.png';
+import play from '../images/play.png'
+import up from '../images/up.png'
+import down from '../images/down.png'
+import refresh from '../images/refresh.png'
+
 
 const Length =({title, changeTime, type, time, formatTime})=>{
     return(
         <div>
-            <h3>{Title}</h3>
+            {/* <h3>{Title}</h3> */}
             <div className="times">
-                <button></button>
+                <img src={up} />
             </div>
         </div>
     )
