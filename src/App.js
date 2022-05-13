7152efef0f7df42fd0385ef0c76f04f49cd0249a
PrincessMaggy <edoziemagdalene@gmail.com>
2022-05-13 14:24:39 +0100

    added session

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, {useState} from 'react';
 import './styles/app.scss';
 
 function App() {
+
+  const [displayTime, setDisplayTime] = useState(25*60);
+
+  const formatTime =(time) =>{
+    let minutes = Math.floor(time/60);
+    let seconds = time% 60;
+    return (minutes< 10? "0" + minutes: minutes ) + ":" + (seconds< 10? "0" + seconds: seconds )
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{formatTime(displayTime)}</h1>
     </div>
   );
 }
